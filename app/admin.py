4f5858cd1f66ce728e445be95da02e1093e380fd
Zarnigor <zarnigorn1255@gmail.com>
2025-01-14 22:15:10 +0500

    Problem model created

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from app.models import Problem
 
-# Register your models here.
+
+@admin.register(Problem)
+class ProblemAdmin(admin.ModelAdmin):
+    pass
--- app/models.py
+++ app/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import Model, ForeignKey, CASCADE
+from django.db.models.enums import TextChoices
+from django.db.models.fields import CharField, TextField, BooleanField
 
-# Create your models here.
+
+class Problem(Model):
+    class Difficulty(TextChoices):
+        HARD = 'HR', 'Hard'
+        MEDIUM = 'MR', 'Medium'
+        EASY = 'EA', 'Easy'
+
+    title = CharField(max_length=255)
+    body = TextField()
+    author = ForeignKey(User, on_delete=CASCADE)
+    difficulty = CharField(max_length=2, choices=Difficulty, default=Difficulty.EASY)
+    is_private = BooleanField(default=False)
